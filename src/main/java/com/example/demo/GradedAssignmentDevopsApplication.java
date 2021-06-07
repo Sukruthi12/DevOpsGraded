906f3bf2ef7dbf2e19f57b73e12e0d992aba24bb
Sukruthi <aletisukruthi12@gmail.com>
2021-06-07 09:00:14 +0530

    first

---
+++ src/main/java/com/example/demo/GradedAssignmentDevopsApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradedAssignmentDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradedAssignmentDevopsApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
